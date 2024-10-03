3dbc6552d17cb53c355a1835453a1b45d7313465
jawid-fakhari <jawid_fakhari@live.it>
2024-10-03 09:52:41 +0200

    Size Class added

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,8 +1,14 @@
+import Sizes from "./Utils/Sizes";
+
 export default class Experience {
   constructor(canvas) {
     //Global access (non indicato)
     window.experience = this;
-    //...
+
+    //Options
     this.canvas = canvas;
+
+    //Setup
+    this.sizes = new Sizes();
   }
 }
