31b07ad28404ccb6d21b004316d13c5fcd88f314
jawid-fakhari <jawid_fakhari@live.it>
2024-10-04 09:18:21 +0200

    some small thing added

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,5 +1,6 @@
-import Experience from "../Experience.js";
 import * as THREE from "three";
+import Experience from "../Experience.js";
+import Environment from "./Environment.js";
 
 export default class World {
   constructor() {
@@ -9,10 +10,11 @@ export default class World {
     //Test mesh
     const testMesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({
-        wireframe: true,
-      })
+      new THREE.MeshStandardMaterial()
     );
     this.scene.add(testMesh);
+
+    //Setup
+    const environment = new Environment();
   }
 }
